b118d6ece57f5118bd0188d4cd075c5be6a18716
Gregory Gerasimov <58705255+HOWLYYY@users.noreply.github.com>
2020-12-25 15:19:48 +0300

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -1,21 +1,28 @@
 function calc {
-if ! (echo "$2" | grep -E -q "^[-+]?[0-9]+$") || ! (echo "$3" | grep -E -q "^[-+]?[0-9]+$")
+if [[ $2 =~ ^[-+]?[0-9]+$ ]] && [[ $3 =~ ^[-+]?[0-9]+$ ]]
 then
        echo "chisla durik!" >&2;
        exit 1
 fi
 case $1 in
-"sum") echo $(($2 + $3))
+"sum") 
+       sum=$(($2 + $3))
+       echo $sum
 ;;
-"sub") echo $(($2 - $3))
+"sub") 
+       sub=$(($2 - $3))
+       echo $sub
 ;;
-"mul") echo $(($2 * $3))
+"mul") 
+       mul=$(($2 * $3))
+       echo $mul
 ;;
 "div") if [[ $3 == "0" || $3 == "-0" || $3 == "+0" ]]
 then
        echo "durah na 0 delit?" >&2;
        else
-       echo $($2 / $3);
+              div=$(($2 / $3)
+              echo $div
        fi
 ;;
 *) echo "ne znayu takih deystviy" >&2
