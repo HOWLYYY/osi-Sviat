a40460e945893d98f0fb83dfc15f17929ae26927
Gregory Gerasimov <58705255+HOWLYYY@users.noreply.github.com>
2020-12-26 11:39:24 +0300

    Update log.sh

--- log.sh
+++ log.sh
@@ -2,20 +2,20 @@ function log {
     log="/var/log/anaconda/X.log"
     check=0
 
-    [ ! -e log ] && echo "No log file" && let $check=1
-    [ ! -r log ] && echo "No rights for reading log file" && let $check=1
+    [ ! -e $log ] && echo "No log file" && let $check=1
+    [ ! -r $log ] && echo "No rights for reading log file" && let $check=1
 
     if [ $check -eq 0 ]
     then
       lines=$(cat $log)
-      for line in lines
+      for line in $lines
       do
       if [[ "$line" == *"WW"* && "$line" != *"(WW) warninig, (EE) error, (NI) not implemented, (??) unknown" ]]
       then
         echo -e ${line//"(WW)"/"\033[33mWarning: \033[0m"}
       fi
       done
-      for line in lines
+      for line in $lines
       do
       if [[ "$line" == *"(II)"* && "$line" != *"(++) from command line, (!!) notice, (II) informational,"* ]]
       then
